5ae14411eeab026dd5cc05c4429a9d61097f0a84
Anthony Lee <ahjlee74@gmail.com>
2023-10-09 20:01:48 -0700

    add

--- app/info.tsx
+++ app/info.tsx
@@ -8,6 +8,7 @@ const Info = () => {
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
         <li><a href="/info">Info</a></li>
+        <li><a href="/info">stuff</a></li>
       </ul>
     </nav>
   );
